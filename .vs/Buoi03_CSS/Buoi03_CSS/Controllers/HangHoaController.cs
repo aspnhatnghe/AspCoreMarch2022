bc99142c5baf1adb393b95a320374386af25e118
NN <pynhatnghe@gmail.com>
2022-03-24 18:43:29 +0700

    Done

---
+++ .vs/Buoi03_CSS/Buoi03_CSS/Controllers/HangHoaController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi03_CSS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi03_CSS.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            //B1: Lấy danh sách hàng hóa
+            var random = new Random();
+            var soLuong = random.Next(5, 100);
+            var danhSachHangHoa = new List<HangHoa>()
+            {
+                //new HangHoa
+                //{
+                //    TenHangHoa = "IPhone 13",
+                //    Hinh = "",
+                //    DonGia = 20990900,
+                //    HangMoi = false
+                //},
+                //new HangHoa
+                //{
+                //    TenHangHoa = "IPhone 14",
+                //    Hinh = "",
+                //    DonGia = 22990900,
+                //    HangMoi = true
+                //},
+            };
+            for (var i = 0; i < soLuong; i++)
+            {
+                danhSachHangHoa.Add(new HangHoa
+                {
+                    TenHangHoa = "IPhone " + random.Next(6, 21),
+                    Hinh = "",
+                    DonGia = random.NextDouble() * 10000000,
+                    HangMoi = random.Next() % 2 == 0
+                });
+            }
+
+            //B2: Chuyển dữ liệu danh sách qua View để hiển thị (gửi qua Model)
+            return View(danhSachHangHoa);
+            //return View("DanhSach", danhSachHangHoa);
+        }
+    }
+}
---
+++ .vs/Buoi03_CSS/Buoi03_CSS/Models/HangHoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi03_CSS.Models
+{
+    public class HangHoa
+    {
+        //Automatic property
+        public string TenHangHoa { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public bool HangMoi { get; set; }
+    }
+}
