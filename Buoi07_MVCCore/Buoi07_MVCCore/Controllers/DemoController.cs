f5f444423f87dde7a6fb7bcc5ef7d2c1ecbfa856
NN <aspcorenn@gmail.com>
2022-03-31 21:27:36 +0700

    Transfer data between Controller and view

---
+++ Buoi07_MVCCore/Buoi07_MVCCore/Controllers/DemoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi07_MVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi07_MVCCore.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nhất Nghệ";
+            ViewBag.NamThanhLap = new DateTime(2003, 3, 10);
+
+            var hangHoa = new Product { 
+                Id = Guid.NewGuid(),
+                ProductName = "Bia Sài Gòn",
+                Price = 14500,
+                Quantity = 24
+            };
+            ViewBag.HangHoa = hangHoa;
+            //TempData["hanghoa"] = hangHoa;
+            TempData["Demo"] = 12345;
+            //return View();
+
+            //return RedirectToAction("Process");
+            //return RedirectToAction("Process", "Demo");
+            //return Redirect("/Demo/Process");
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Process()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ Buoi07_MVCCore/Buoi07_MVCCore/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi07_MVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi07_MVCCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> Products = new List<Product>()
+        {
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                ProductName = "Bia 333",
+                Price = 15900,
+                Quantity =23
+            },
+            new Product
+            {
+                Id = Guid.NewGuid(),
+                ProductName = "Bia Heineiken",
+                Price = 19500,
+                Quantity =29
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(Products);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            //LINQ, lambda
+            var product = Products.SingleOrDefault(item => item.Id == id);
+
+            if (product == null)//không có
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Guid id, Product model)
+        {
+            var product = Products.SingleOrDefault(item => item.Id == id);
+            if (product != null)//có
+            {
+                product.ProductName = model.ProductName;
+                product.Price = model.Price;
+                product.Quantity = model.Quantity;
+
+                //return RedirectToAction("Index");
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
