2f4ca2368b260c4eec28506c181468a426f1578a
NN <aspcorenn@gmail.com>
2022-04-02 19:07:03 +0700

    Lab06 - StudentInfo model

---
+++ Buoi07_MVCCore/Buoi07_MVCCore/Models/StudentInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi07_MVCCore.Models
+{
+    public class StudentInfo
+    {
+        [Display(Name ="Mã sinh viên")]
+        [Required(ErrorMessage ="*")]
+        public string StudentId { get; set; }
+
+        [Display(Name = "Họ tên sinh viên")]
+        [Required(ErrorMessage = "*")]
+        [MinLength(5, ErrorMessage ="Tối thiểu 5 kí tự")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Điểm trung bình")]
+        [Range(0, 10, ErrorMessage ="Điểm từ 0 --> 10")]
+        public double Grade { get; set; }
+
+        [Display(Name = "Xếp loại")]
+        public string Rating { get; set; }
+    }
+}
