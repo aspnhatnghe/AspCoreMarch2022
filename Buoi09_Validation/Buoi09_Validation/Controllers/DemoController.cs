beea714a212552f699cb49cb0772bf1875bfcdc5
NN <aspcorenn@gmail.com>
2022-04-05 19:08:31 +0700

    Demo Sync

---
+++ Buoi09_Validation/Buoi09_Validation/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi09_Validation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi09_Validation.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult DemoSync()
+        {
+            var demo = new Demo();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            demo.Test01(); demo.Test02(); demo.Test03();
+            stopWatch.Stop();
+            return Content($"Chạy hết {stopWatch.ElapsedMilliseconds}ms.");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Buoi09_Validation/Buoi09_Validation/Models/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Buoi09_Validation.Models
+{
+    public class Demo
+    {
+        public string Test01()
+        {
+            Thread.Sleep(2000);
+            return "Test01 - 2s";
+        }
+
+        public int Test02()
+        {
+            Thread.Sleep(5000);
+            return 123;
+        }
+        public void Test03()
+        {
+            Thread.Sleep(3000);
+        }
+    }
+}
