4ab52371b1d369f7f850d6eb83be325d60f8ce51
namdongvando <32663464+namdongvando@users.noreply.github.com>
2022-04-23 20:57:57 +0700

    ADO.NET

---
+++ Buoi15_ADONET/Buoi15_ADONET/Controllers/DemoController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Buoi15_ADONET.Controllers
+{
+    public class DemoController : Controller
+    {        
+        public DemoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ChuoiKetNoi = _configuration.GetConnectionString("EStore");
+        }
+
+        string ChuoiKetNoi = string.Empty;
+        private readonly IConfiguration _configuration;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DocDatabase()
+        {
+            var sql = "SELECT * FROM Loai ORDER BY TenLoai";
+
+            SqlConnection connection = new SqlConnection(ChuoiKetNoi);
+
+            SqlDataAdapter da = new SqlDataAdapter(sql, connection);
+
+            DataTable dtLoai = new DataTable();
+            da.Fill(dtLoai);
+
+            //Xử lý kết quả
+            var sb = new StringBuilder();
+            foreach (DataRow dr in dtLoai.Rows)
+            {
+                sb.AppendLine($"{dr[0]} - {dr["TenLoai"]}");
+            }
+
+            return Content(sb.ToString());
+        }
+
+
+        public IActionResult ThemLoai(string TenLoai, string MoTa)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ChuoiKetNoi);
+
+                var sql = $"INSERT INTO Loai(TenLoai, MoTa) VALUES(N'{TenLoai}', N'{MoTa}')";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+                //cmd.Connection.Open();
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                return Content("Thêm thành công");
+            }
+            catch (Exception ex)
+            {
+                return Content($"Lỗi: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Buoi15_ADONET/Buoi15_ADONET/Models/LoaiDataAccessLayer.cs
@@ -0,0 +1,82 @@
+﻿using ASPCore.ADONETLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi15_ADONET.Models
+{
+    public class LoaiDataAccessLayer
+    {
+        public static List<Loai> GetAll()
+        {
+            var dtLoai = DataProvider.SelectData("spLayTatCaLoai", CommandType.StoredProcedure, null);
+            var result = new List<Loai>();
+
+            foreach (DataRow dr in dtLoai.Rows)
+            {
+                result.Add(new Loai
+                {
+                    MaLoai = int.Parse(dr["MaLoai"].ToString()),
+                    TenLoai = dr["TenLoai"].ToString(),
+                    MoTa = dr["MoTa"].ToString(),
+                    Hinh = dr["Hinh"].ToString()
+                });
+            }
+
+            return result;
+        }
+
+        public static Loai GetById(int id)
+        {
+            var pa = new SqlParameter[1]
+            {
+                new SqlParameter("MaLoai", id)
+            };
+            var loai = DataProvider.SelectData("spLayLoai", CommandType.StoredProcedure, pa);
+            if(loai.Rows.Count == 0)
+            {
+                return null;
+            }
+            var dr = loai.Rows[0];
+            return new Loai
+            {
+                MaLoai = int.Parse(dr["MaLoai"].ToString()),
+                TenLoai = dr["TenLoai"].ToString(),
+                MoTa = dr["MoTa"].ToString(),
+                Hinh = dr["Hinh"].ToString()
+            };
+        }
+
+        public static Loai Add(Loai loai)
+        {
+            try
+            {
+                var pa = new SqlParameter[4]
+                {
+                new SqlParameter("MaLoai", SqlDbType.Int),
+                new SqlParameter("TenLoai", loai.TenLoai),
+                new SqlParameter("MoTa", loai.MoTa),
+                new SqlParameter("Hinh", loai.Hinh)
+                };
+                pa[0].Direction = ParameterDirection.Output;
+
+                DataProvider.ExcuteNonQuery("spThemLoai", CommandType.StoredProcedure, pa);
+
+                loai.MaLoai = int.Parse(pa[0].Value.ToString());
+                return loai;
+            }
+            catch { return null; }
+        }
+
+        //public static Loai Update(Loai loai)
+        //{
+
+        //}
+
+        //public static bool Delete(int id)
+        //{ }
+    }
+}
