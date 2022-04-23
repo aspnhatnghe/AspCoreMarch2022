e3efa42ddb4b77dcb02b193cb31fddda43a30144
namdongvando <32663464+namdongvando@users.noreply.github.com>
2022-04-23 21:27:59 +0700

    Create Category

---
+++ Buoi15_ADONET/Buoi15_ADONET/Controllers/LoaiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buoi15_ADONET.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi15_ADONET.Controllers
+{
+    public class LoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(LoaiDataAccessLayer.GetAll());
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var loai = LoaiDataAccessLayer.GetById(id);
+            if (loai != null)
+            {
+                return View(loai);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Loai loai)
+        {
+            var newLoai = LoaiDataAccessLayer.Add(loai);
+            if (newLoai == null)
+            {
+                return View();
+            }
+            //return RedirectToAction("Index");
+            return RedirectToAction("Edit", new { id = newLoai.MaLoai });
+        }
+    }
+}
