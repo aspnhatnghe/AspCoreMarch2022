b05f49a79accb1e6d6d5fb2b0efb9fffa76ef2f5
NN <pynhatnghe@gmail.com>
2022-04-26 21:29:18 +0700

    Done day 16

---
+++ Buoi16_EFCore/Buoi16_EFCore/Entities/Loai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi16_EFCore.Entities
+{
+    [Table("Loai")]
+    public class Loai
+    {
+        [Key]
+        public int MaLoai { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+        public string MoTa { get; set; }
+        public string Hinh { get; set; }
+    }
+}
---
+++ Buoi16_EFCore/Buoi16_EFCore/Entities/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buoi16_EFCore.Entities
+{
+    public class MyDbContext : DbContext
+    {
+        #region Properties
+        public DbSet<Loai> Loais { get; set; }
+        public DbSet<HangHoa> HangHoas { get; set; }
+        #endregion
+
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
