b94bb77a7dcb2a3821fff4073caa5fc5ae9fc2bc
NN <pynhatnghe@gmail.com>
2022-05-28 20:56:54 +0700

    Add CategoryHorizontalMenu

---
+++ FinalProject/FinalProject/ViewComponents/CategoryHorizontalMenu.cs
@@ -0,0 +1,24 @@
+﻿using FinalProject.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewComponents
+{
+    public class CategoryHorizontalMenu : ViewComponent
+    {
+        private readonly ShopDbContext _context;
+
+        public CategoryHorizontalMenu(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Categories.ToList());
+        }
+    }
+}
