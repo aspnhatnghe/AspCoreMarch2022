c7f95d7b9b713897c1bf9f80db691e5f6d58786d
NN <pynhatnghe@gmail.com>
2022-06-02 19:36:24 +0700

    Customer Model

---
+++ FinalProject/FinalProject/Data/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Data
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        [MaxLength(150)]
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsActive { get; set; }
+        [MaxLength(30)]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string RandomKey { get; set; }
+    }
+}
